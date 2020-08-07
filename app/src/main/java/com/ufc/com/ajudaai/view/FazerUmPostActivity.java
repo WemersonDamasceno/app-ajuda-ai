cbc8d7bfdd306453aff8a5e0267fb547b8c8dd3b
Wemerson7 <wemerson_damasceno@yahoo.com>
2020-08-07 15:49:28 -0300

    Foi feita a tela de criar postagem

---
+++ app/src/main/java/com/ufc/com/ajudaai/view/FazerUmPostActivity.java
@@ -0,0 +1,25 @@
+package com.ufc.com.ajudaai.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ufc.com.ajudaai.R;
+
+public class FazerUmPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fazer_um_post);
+
+
+
+        //Quando fizer o upload do PDF mudar o icon branco para o PDFCompleto
+        //O texto nao poder ter mais que 175 caracteres.
+
+
+
+
+    }
+}
--- app/src/main/java/com/ufc/com/ajudaai/view/Pagina_Inicial_Activity.java
+++ app/src/main/java/com/ufc/com/ajudaai/view/Pagina_Inicial_Activity.java
@@ -1,26 +1,38 @@
 package com.ufc.com.ajudaai.view;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.ufc.com.ajudaai.R;
 
 public class Pagina_Inicial_Activity extends AppCompatActivity {
     Toolbar toolbar;
+    TextView txtEscreverAqui;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_inicial);
         getSupportActionBar().hide();
+
+
+        txtEscreverAqui = findViewById(R.id.txtEscrevaAqui);
+        txtEscreverAqui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(),FazerUmPostActivity.class));
+            }
+        });
+
+
+
 
     }
     private void verificarAutenticado() {
